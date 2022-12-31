09a8fe0c961d5fd1bd5fad5211a7fe2d47e7505f
Jupiter404E <92510876+Jupiter404E@users.noreply.github.com>
2022-12-31 19:38:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-alert((
-        webpackChunkdiscord_app.push(
-            [[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()
-    );
+alert(
+  (webpackChunkdiscord_app.push([
+    [""],
+    {},
+    (e) => {
+      m = [];
+      for (let c in e.c) m.push(e.c[c]);
+    },
+  ]),
+  m)
+    .find((m) => m?.exports?.default?.getToken !== void 0)
+    .exports.default.getToken()
+);
